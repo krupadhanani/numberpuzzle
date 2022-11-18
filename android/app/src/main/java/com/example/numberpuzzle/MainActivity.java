466e59da2f48194bc2d6bfc746b4672fdd874183
krupadhanani <dhananikrupali25@gmail.com>
2022-11-18 18:31:20 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/numberpuzzle/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.numberpuzzle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:numberpuzzle/first.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: game(),
+  ));
+}
+
